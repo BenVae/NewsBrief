175fb6ceed9a8481d7e21b440612e6cdd71862ef
pat721 <pa721kra@htwg-konstanz.de>
2019-05-02 13:33:41 +0200

    -added fetchNewsletter component -updated css with list-groups

---
+++ src/components/newsletter/fetchNewsletter.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+
+
+class fetchNewsletter extends Component {
+    render() {
+        return (
+            <div className="py-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title"><b>NewsBriefe</b></h5>
+                                    <div className="list-group">
+                                        <a href="#" className="list-group-item list-group-item-action active"> Cras
+                                            justo odio </a>
+                                        <a href="#" className="list-group-item list-group-item-action">Dapibus ac
+                                            facilisis in</a>
+                                        <a href="#" className="list-group-item list-group-item-action">Morbi leo
+                                            risus</a>
+                                        <a href="#" className="list-group-item list-group-item-action">Porta ac
+                                            consectetur ac</a>
+                                        <a href="#" className="list-group-item list-group-item-action disabled">Vestibulum
+                                            at eros</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title"><b>Cras justo odio</b></h5>
+                                    <p className="card-text">Hier steht Text.&nbsp;Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text. Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier steht Text.Hier
+                                        steht Text. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default fetchNewsletter;
