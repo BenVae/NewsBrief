97b554070a34003d96de584ecae3382982fb5901
pat721 <pa721kra@htwg-konstanz.de>
2019-05-06 13:54:47 +0200

    - added customer Testdata - the customerlist is now generated by loading data

--- src/components/message/SearchCustomerCardBody.js
+++ src/components/message/SearchCustomerCardBody.js
@@ -1,7 +1,25 @@
 import React, {Component} from 'react';
+import {fetchCustomerdata} from "../../helper/FetchData";
 
 
 export default class SearchCustomerCardBody extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            customers: []
+        };
+    }
+
+    componentWillMount() {
+        let testdata = fetchCustomerdata();
+
+        this.setState({
+            customers: testdata
+        })
+    }
+
     render() {
         return (
             <div className="card-body border-bottom">
@@ -25,15 +43,16 @@ export default class SearchCustomerCardBody extends Component {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>4234234, Dieter</td>
-                        </tr>
-                        <tr>
-                            <td>2342345, Peter</td>
-                        </tr>
-                        <tr>
-                            <td>2131236, Jgurtz</td>
-                        </tr>
+                        {
+                            this.state.customers.map((customer, index) => (
+                                <tr>
+                                    <td key={index} id={customer.customerId}
+                                        className="list-group-item list-group-item-action td-list-item clickable-anchor-tags">
+                                        {customer.customerId} {customer.surname} {customer.name}
+                                    </td>
+                                </tr>
+                            ))
+                        }
                         </tbody>
                     </table>
                 </div>
--- src/helper/FetchData.js
+++ src/helper/FetchData.js
@@ -1,4 +1,5 @@
 import newsletterTestdata from "./developmentdata/newsletterTestdata.json";
+import customerTestdata from "./developmentdata/customerTestdata";
 import {AxiosInstance as axios} from "axios";
 
 export const fetchNewsletterData = function() {
@@ -21,4 +22,26 @@ export const fetchNewsletterData = function() {
                 alert(error.toString());
             })
     }
-}
+};
+
+export const fetchCustomerdata = function() {
+    if (process.env.NODE_ENV !== 'production') {
+
+        return customerTestdata;
+
+    } else {
+
+        axios.get('addDefaultURL')
+            .then(response => {
+                this.setState({
+                    newsletters: response.data,
+                    title: response.data[0].title,
+                    content: response.data[0].content,
+                    itemCurrentlyActive: response.data[0].newsletterId
+                })
+            })
+            .catch(error => {
+                alert(error.toString());
+            })
+    }
+};
